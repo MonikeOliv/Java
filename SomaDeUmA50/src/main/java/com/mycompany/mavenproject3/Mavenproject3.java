77fe4d7c98ce9426f074bbe238d1b291cceeac5e
MonikeOliv <monike17gon@gmail.com>
2024-10-09 19:45:35 -0300

    Add files via upload
    Atv Soma De Um A 50

---
+++ SomaDeUmA50/src/main/java/com/mycompany/mavenproject3/Mavenproject3.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject3;
+
+/**
+ *
+ * @author Monike 
+ */
+public class Mavenproject3 {
+
+public static void main(String[] args) {
+    int soma = 0; // Inicialização
+    for (int i = 1; i <= 50; i++) {
+            soma += i; // Adiciona o valor de i à variável soma
+            }
+System.out.println("A soma dos números de 1 a 50 é: " + soma);
+    }
+}
+
