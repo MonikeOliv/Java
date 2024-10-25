d8fbe74ddc0b23228193cf6000aea121292ed2b3
MonikeOliv <monike17gon@gmail.com>
2024-10-25 13:11:55 -0300

    Add files via upload

--- Tabuada/src/main/java/com/mycompany/tabuada/Tabuada.java
+++ Tabuada/src/main/java/com/mycompany/tabuada/Tabuada.java
@@ -4,26 +4,26 @@
 
 package com.mycompany.tabuada;
 
+import java.util.Scanner;
+
 /**
  *
- * @author Monike
+ * @author Monike Oliveira
  */
-import java.util.Scanner;
 public class Tabuada {
-    public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("Informe um número de 1 a 10: ");
-            int numero = scanner.nextInt();
-            
-            if (numero < 1 || numero > 10) {
-                System.out.println("Número inválido! Informe um número entre 1 e 10.");
-            } else {
-                System.out.println("Tabuada de " + numero + ":");
-                for (int i = 1; i <= 10; i++) {
-                    System.out.println(numero + " x " + i + " = " + (numero * i));
-                }
-            }
+
+public static void main(String[] args) {
+    try (Scanner scanner = new Scanner(System.in)) {
+        System.out.print("Digite um número para ver sua tabuada: ");
+        int numero = scanner.nextInt();
+        int contador = 1;
+
+        System.out.println("Tabuada do " + numero + ":");
+        while (contador <= 10) {
+            int resultado = numero * contador;
+            System.out.println(numero + " x " + contador + " = " + resultado);
+            contador++;
         }
     }
+    }
 }
-
