50d13267f35bc66578e732467c40fa248e2cea4f
MonikeOliv <monike17gon@gmail.com>
2025-03-25 22:06:03 -0300

    Add files via upload

---
+++ SistemaGenero/src/main/java/com/mycompany/sistemagenero/SistemaGenero.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemagenero;
+
+/**
+ *
+ * @author Admin
+ */
+public class SistemaGenero {
+
+    public static void main(String[] args) {
+        Pessoa p1 = new Pessoa("Monike", "F");
+        p1.sexoDescricao();
+        p1.exibirInformacao();
+    }
+}
